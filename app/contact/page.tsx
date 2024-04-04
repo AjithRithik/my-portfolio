c94fbad7a41789672a0818911da0cf18b60f53e2
Ajithkumar <rithik171995@gmail.com>
2024-04-04 14:41:38 +0530

    fix: special character fixes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,7 +11,7 @@ export default function Contact() {
       <Heading title="Contact" />
       <div className="rounded-lg mt-12 bg-blue-50 p-12">
         <div className="text-3xl font-light mb-6">
-          I'm Always Open to Discuss <span className="font-semibold text-red-400">Frontend Development Work.</span> 
+          {`I'm Always Open to Discuss`} <span className="font-semibold text-red-400">Frontend Development Work.</span> 
         </div>
         <div className="grid grid-cols-2 gap-6">
           <div className="flex flex-1">
