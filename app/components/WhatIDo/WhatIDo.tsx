a54953ead8af5e144ffe50e9991f7c56e27b9f3d
Ajithkumar <rithik171995@gmail.com>
2024-04-10 15:43:18 +0530

    fix: some mobile responsive fixes

--- app/components/WhatIDo/WhatIDo.tsx
+++ app/components/WhatIDo/WhatIDo.tsx
@@ -1,23 +1,21 @@
 import { ReactNode } from "react";
 
 export type WhatIDoProps = {
-    icon: ReactNode;
-    title: string;
-    desc: string;
-    className?: string;
-}
+  icon: ReactNode;
+  title: string;
+  desc: string;
+  className?: string;
+};
 
-export const WhatIDo = ({icon, title, desc, className}: WhatIDoProps) => {
+export const WhatIDo = ({ icon, title, desc, className }: WhatIDoProps) => {
   return (
-    <div className={`flex flex-1 p-4  rounded-xl gap-4 mt-4 ${className}`}>
-      <div className="text-blue-400 text-3xl">
-        {icon}
-      </div>
-      <div className="flex flex-col gap-2">
-        <div className="font-semibold">{title}</div>
-        <div className="text-[13px] text-gray-700">
-          {desc}
-        </div>
+    <div
+      className={`flex flex-col sm:flex-row items-center sm:items-start flex-1 p-4 rounded-xl gap-6 md:gap-4 mt-4 ${className}`}
+    >
+      <div className="text-blue-400 text-5xl md:text-3xl">{icon}</div>
+      <div className="flex items-center sm:items-start flex-col gap-2">
+        <div className="font-semibold text-center sm:text-left">{title}</div>
+        <div className="text-[13px] text-gray-700 text-justify sm:text-left">{desc}</div>
       </div>
     </div>
   );
